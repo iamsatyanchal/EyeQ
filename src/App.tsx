89cfeccbe608b0920fcfa219222e7042e17ad846
Satyanchal Chaudhary <62104921+iamsatyanchal@users.noreply.github.com>
2026-05-13 00:33:41 +0530

    Update App component with header and main structure
    Refactor App component layout and header structure.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import eyes_data from "./data/eyes.json";
 export default function App(){
   return (
     <>
-    {
+    <main className="h-full w-full bg-black font-sans text-white">
+      <header className="h-14 flex items-center justify-center px-6 border-b border-white/20 bg-black z-[100] shrink-0 sticky top-0">
+        <p className="font-display text-2xl tracking-wide font-bold uppercase block">
+          ~ EyeQ ~
+          </p>
+      </header>
+    {/* {
       eyes_data.map((item, index) => (
         <div key={index}>
           <img src={item.image_url}/>
@@ -15,7 +21,8 @@ export default function App(){
           <p>right option: {item.answer}</p>
         </div>
       ))
-    }
+    } */}
+    </main>
     </>
   )
 }
