7065621821d82913d29f2dc085c3963d9a3142dc
Satyanchal Chaudhary <62104921+iamsatyanchal@users.noreply.github.com>
2026-05-13 18:00:37 +0530

    Refactor App component layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -23,15 +23,15 @@ export default function App(){
     }
   return (
     <>
-    <main className="h-screen w-full bg-black font-sans text-white">
+    <main className="h-screen w-full bg-black font-sans text-white flex flex-col">
       <header className="h-14 flex items-center justify-center px-6 border-b border-white/20 bg-black z-[100] shrink-0 sticky top-0">
-        <p className="font-display text-2xl tracking-wide font-bold uppercase block">
-          ~ EyeQ ~
+        <p className="font-display text-2xl tracking-wide font-bold block">
+          EYEQ <span className="md:inline-block hidden">~ Can you guess the emotions through the eyes?</span>
           </p>
       </header>
 
-<section className="relative bg-black flex-1 overflow-hidden items-center">
-  <img src={eyes_data[rand_image].image_url} className="w-full h-full object-cover"/>
+<section className="flex-1 relative bg-black flex overflow-hidden items-center justify-center">
+  <img src={eyes_data[rand_image].image_url} className="flex items-center w-full h-full object-contain"/>
 </section>
 <section className="h-12 bg-black z-[100]">
 <div className="grid grid-cols-4 flex items-center h-12 w-full divide-x divide-white/20 fixed bottom-0 border-t border-white/20">
@@ -39,7 +39,7 @@ export default function App(){
   eyes_data[rand_image].options.map((option)=> (
     <button 
     key={option}
-    className={`text-center h-full capitalize tracking-wide text-lg outline-none transition-all duration-200
+    className={`text-center h-full capitalize tracking-wide text-lg font-semibold outline-none transition-all duration-200
     ${selected == option ? colour : ""}`} onClick={()=> check_option(option)} >{option} <span>
       {selected == option && option == eyes_data[rand_image].answer ? "✓" : selected == option && option != eyes_data[rand_image].answer ? "✗" : ""}
       </span></button>
